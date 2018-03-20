0a3dfb1e07745014b0483b810ad4016b56b359df
Jacob Fletcher <jacobsfletch@gmail.com>
2018-03-20 12:42:05 -0400

    simplifies svg datasets

--- src/Chart.js
+++ src/Chart.js
@@ -7,18 +7,18 @@ import './index.css';
 class Chart extends Component {
 	constructor(props) {
 		super(props);
-		this.arrayX = [44.5, 84.5, 124.5, 164.5, 204.5, 244.5, 284.5, 324.5, 364.5, 404.5, 444.5, 484.5, 524.5, 564.5, 604.5, 644.5, 684.5, 724.5, 764.5]
-		this.arrayY = [5, 30, -5, -10, 15, -15, 20, 5, 8, -12, -20, 2, 3, -5, 8, -2, 22, -30, -22]
+		this.arrayX = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000];
+		this.arrayY = [5, 30, -5, -10, 15, -15, 20, 5, 8, -12, -20, 2, 3, -5, 8, -2, 22, -30, -15, -35, -20];
 	}
 
 	componentWillMount() {
-		this.generatePolylineArray()
+		this.generatePolylineArray();
 	}
 
 	generatePolylineArray() {
 		let polyline = '';
 		this.arrayX.map((coordX, i) => {
-			return polyline += `${coordX}, ${this.arrayY[i]} `;
+			return polyline += `${coordX},${this.arrayY[i]} `;
 		})
 		this.polyline = polyline;
 	}
